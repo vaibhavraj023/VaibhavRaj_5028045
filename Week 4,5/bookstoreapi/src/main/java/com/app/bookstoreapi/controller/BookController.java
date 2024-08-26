4517053bb613f35907eac1028602ea2c44f73d03
vaibhavraj023 <vaibhavrajyoo@gmail.com>
2024-08-26 09:52:38 +0530

    Ex 12 done

--- Week 4,5/bookstoreapi/src/main/java/com/app/bookstoreapi/controller/BookController.java
+++ Week 4,5/bookstoreapi/src/main/java/com/app/bookstoreapi/controller/BookController.java
@@ -3,9 +3,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.app.bookstoreapi.entity.Book;
 import com.app.bookstoreapi.service.BookService;
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,43 +26,44 @@ public class BookController {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
     @GetMapping("/{id}")
-    public Optional<Book> getBookById(@PathVariable Long id){
+    public ResponseEntity<Object> getBookById(@PathVariable Long id){
         return bookService.getBookById(id);
     }
     @GetMapping("/title/{title}")
-    public Optional<Book> getBookByTitle(@PathVariable String title){
+    public ResponseEntity<Object> getBookByTitle(@PathVariable String title){
         return bookService.getBookByTitle(title);
     }
     @GetMapping("/author/{author}")
-    public List<Book> getBookByAuthor(@PathVariable String author){
+    public ResponseEntity<Object> getBookByAuthor(@PathVariable String author){
         return bookService.getBookByAuthor(author);
     }
     @GetMapping("/price/{price}")
-    public List<Book> getBookByPrice(@PathVariable Double price){
+    public ResponseEntity<Object> getBookByPrice(@PathVariable Double price){
         return bookService.getBookByPrice(price);
     }
     @GetMapping("/isbn/{isbn}")
-    public Book getBookByIsbn(@PathVariable String isbn){
+    public ResponseEntity<Object> getBookByIsbn(@PathVariable String isbn){
         return bookService.getBookByIsbn(isbn);
     }
     @PostMapping
-    public ResponseEntity<Book> insertBook(@RequestBody Book book){
-        Book new_book=bookService.saveBook(book);
-        return ResponseEntity.created(URI.create("/books/"+new_book.getId())).body(new_book);
+    public ResponseEntity<String> insertBook(@RequestBody Book book){
+        return bookService.saveBook(book);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book){
-        Book updatedBook=bookService.updateBook(id,book);
-        return ResponseEntity.ok(updatedBook);
+    public ResponseEntity<String> updateBook(@PathVariable Long id, @RequestBody Book book){
+        return bookService.updateBook(id,book);
     }
     @PatchMapping("/{id}")
-    public ResponseEntity<Book> updateBookPrice(@PathVariable Long id, @RequestBody Double price){
-        Book updatedBook=bookService.updateBookPrice(id,price);
-        return ResponseEntity.ok(updatedBook);
+    public ResponseEntity<String> updateBookPrice(@PathVariable Long id, @RequestBody Book book){
+        return bookService.updateBookPrice(id,book);
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteBook(@PathVariable Long id){
-        bookService.deleteBook(id);
-        return ResponseEntity.ok("Book deleted successfully !");
+    public ResponseEntity<Object> deleteBookById(@PathVariable Long id){
+        return bookService.deleteBookById(id);
+    }
+    @DeleteMapping
+    public ResponseEntity<String> deleteBook(){
+        bookService.deleteBook();
+        return ResponseEntity.ok("All books deleted successfully");
     }
 }
--- Week 4,5/bookstoreapi/src/main/java/com/app/bookstoreapi/repo/BookRepository.java
+++ Week 4,5/bookstoreapi/src/main/java/com/app/bookstoreapi/repo/BookRepository.java
@@ -1,7 +1,6 @@
 package com.app.bookstoreapi.repo;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,9 +12,9 @@ public interface BookRepository extends JpaRepository<Book,Long> {
     @Query(name = "Book.findByTitle")
     Optional<Book> findByTitle(@Param("title") String title);
     @Query("SELECT b FROM Book b WHERE b.author=:author")
-    List<Book> findByAuthor(@Param("author") String author);
+    Optional<List<Book>> findByAuthor(@Param("author") String author);
     @Query("select b from Book b where b.price=:price")
-    List<Book> findByPrice(Double price);
+    Optional<List<Book>> findByPrice(Double price);
     @Query("select b from Book b where b.isbn=:isbn")
-    Book findByIsbn(String isbn);
+    Optional<Book> findByIsbn(String isbn);
 }
